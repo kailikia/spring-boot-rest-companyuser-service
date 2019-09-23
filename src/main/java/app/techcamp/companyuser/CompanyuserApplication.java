776df83010a827bcbf5bd2a548d3951e884883fa
tech-camp <edwinkailikia@gmail.com>
2019-09-23 21:29:32 +0300

    jpa audit added

--- src/main/java/app/techcamp/companyuser/CompanyuserApplication.java
+++ src/main/java/app/techcamp/companyuser/CompanyuserApplication.java
@@ -2,8 +2,10 @@ package app.techcamp.companyuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CompanyuserApplication {
 
 	public static void main(String[] args) {
